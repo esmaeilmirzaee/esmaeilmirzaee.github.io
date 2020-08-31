14d231cd1dffefe658f0e1c4739802d6ae527972
EsmaeilMIRZAEE <EsmaeilMIRZAEE@gmail.com>
2020-08-31 10:35:34 -0400

    Updates

---
+++ precache-manifest.730992bebe607e385cad8f722deaeb43.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f330ff2737d848d471a4fde000bbc0af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd2e7ceecb9f457c7fa9",
+    "url": "/static/css/main.d36c0ff5.chunk.css"
+  },
+  {
+    "revision": "b2772aa4865919f2705b",
+    "url": "/static/js/2.750ee5b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.750ee5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2e7ceecb9f457c7fa9",
+    "url": "/static/js/main.fa8a5c41.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "58d6481215735c8718c9c273b56004b3",
+    "url": "/static/media/BrandStock.58d64812.png"
+  },
+  {
+    "revision": "d5a513aad281d1e0f778bb4b77aaf0a1",
+    "url": "/static/media/MelborneCity.d5a513aa.mp4"
+  },
+  {
+    "revision": "5f1116f9f346a18883bd1fa3b9b1508e",
+    "url": "/static/media/ds_logo_ai.5f1116f9.png"
+  },
+  {
+    "revision": "cfb9bf3dfd66196e144f95c64816ea33",
+    "url": "/static/media/ds_portfolio_figma.cfb9bf3d.png"
+  },
+  {
+    "revision": "888ea9194c6cfd7b5c7a8794e7824bc6",
+    "url": "/static/media/hero__mac.888ea919.svg"
+  },
+  {
+    "revision": "e2c003683dbb1f24c831b87761c65b6d",
+    "url": "/static/media/iMacDesktop.e2c00368.png"
+  },
+  {
+    "revision": "6eb44f8bab7cfdb76b7f96e4d69a094a",
+    "url": "/static/media/lee-campbell-6njoEbtarec-unsplash.6eb44f8b.jpg"
+  },
+  {
+    "revision": "2a83d862d1dd78f7aea893e85fc941c8",
+    "url": "/static/media/logo.2a83d862.svg"
+  },
+  {
+    "revision": "077c3b20f65d933791b9d981a80a4c48",
+    "url": "/static/media/my__avatar.077c3b20.svg"
+  },
+  {
+    "revision": "afd9e3453a3ef8c43128dc123e69d06e",
+    "url": "/static/media/vbox__logo.afd9e345.svg"
+  }
+]);
